761b44a36e8508b0f9190773bb7af16bd4bc6a34
DeepSea3474 <redbluealking@hotmail.com>
2026-03-12 00:16:52 +0100

    Import assignTask from autonomousScheduler

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import { assignTask } from "./autonomousScheduler";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
